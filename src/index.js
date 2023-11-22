47955deb1e282f276d0ef3309403beb8adb09864
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-22 14:59:19 +0100

    Update event button to add count

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ const list = document.querySelector(".list")
 const btnCount = document.querySelector(".count")
 
 
-
+function updateCount() {
+    const currentText = btnCount.textContent
+    const currentCount = parseInt(currentText.split(' ')[1], 10);
+    const updatedCount = currentCount + 1;
+    btnCount.textContent = `COUNT ${updatedCount}`;
+}
 
 function getRandomColors() {
     const letters = '0123456789ABCDEF';
     let color ='#';
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-        console.log(color);
-        
+        color += letters[Math.floor(Math.random() * 16)]
     }
     return color;
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const body = document.body;
-//     body.style.backgroundColor = getRandomColors();
-//     console.log(body.children)
-// }) 
-
 btnCount.addEventListener("click", function() {
     const body = document.body;
-    body.style.backgroundColor = getRandomColors();
+    body.style.backgroundColor = getRandomColors()
+    updateCount();
+
+    
+    
 })
 
 
