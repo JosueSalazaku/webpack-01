d2871f80936a5c1d47d854cc2fdb56e571b61e5e
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-22 14:39:14 +0100

    Add button event to update random page color

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./style.scss";
 
 
 const list = document.querySelector(".list")
+const btnCount = document.querySelector(".count")
 
-console.log(getRandomColors);
 
 
 
@@ -12,15 +12,22 @@ function getRandomColors() {
     let color ='#';
     for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
+        console.log(color);
+        
     }
     return color;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
+//     const body = document.body;
+//     body.style.backgroundColor = getRandomColors();
+//     console.log(body.children)
+// }) 
+
+btnCount.addEventListener("click", function() {
     const body = document.body;
     body.style.backgroundColor = getRandomColors();
-}) 
-
+})
 
 
 
