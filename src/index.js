a576c78d4418d7244c17761cf4ed8a4b116f2305
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-22 14:08:27 +0100

    Update webpack bundler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import "./style.scss";
 
-console.log("Hello, webpack!");
-
+const body = document.querySelector("body")
 const list = document.querySelector(".list")
+
+console.log(getRandomColors);
+
+
+
+function getRandomColors() {
+    return "#" + Math.floor(Math.random() * 116777215).toString(16)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    body.style.backgroundColor = getRandomColors()
+}) 
+
+
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 
 module.exports = {
-    entry: './src/index.js', // Update with your entry file
+    mode: 'development', 
+    entry: './src/index.js',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
